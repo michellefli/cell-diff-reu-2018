5078f449d5a05cf0d83c36c02fe0f3dcb5720a54
Michelle Li <Michelle@WL-223-229.WPA.Claremont.Edu>
2018-06-28 14:56:34 -0700

    removed old/unnecessary files, added my-tsne and my-diff-map folder

--- diff-map/destiny-master/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(destiny)
-
-test_check('destiny')
--- diff-map/destiny-master/tests/testthat/test_basic.r
+++
@@ -1,5 +0,0 @@
-test_that('DiffusionMap works', {
-	data(guo_norm)
-	dm <- DiffusionMap(guo_norm)
-	expect_identical(dm@distance, 'euclidean')
-})
